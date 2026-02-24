3a2736d2d94c078b5aab500ef6addf826dde909a
Colby Poag <72989636+masterpoag@users.noreply.github.com>
2026-02-24 17:32:31 -0600

    Setup routers and fixed packages

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div >
+      <h1>Welcome to test</h1>
+      <p>
+        This is a project created for CSCI 387, a course on software engineering.
+        The project is a web application that allows users to create and share recipes.
+      </p>
+    </div>
+  );
+}
