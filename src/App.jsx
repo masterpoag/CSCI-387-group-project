0043e49c8c971598f531fa6b5c22dcf7af60951a
colby <colby.poag@gmail.com>
2026-03-03 13:00:12 -0600

    added navlink for testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,15 @@ import LoginPage from "./pages/loginpage.jsx"
 function App() {
   return (
     <Router basename="/~cgpoag/CSCI-387">
+    <nav style={navStyle}>
+        {/* Navigation links */}
+        <NavLink to="/">
+          HomePage
+        </NavLink>
+        <NavLink to="/login">
+          LoginPage
+        </NavLink>
+      </nav>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
